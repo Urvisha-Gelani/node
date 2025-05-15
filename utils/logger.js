87791dbe5deee1dfffe0e816178f23a3ae177a90
Urvisha-Gelani <hirapraurvisha@gmail.com>
2025-05-15 17:02:51 +0530

    add logs

--- utils/logger.js
+++ utils/logger.js
@@ -1,21 +1,29 @@
 import winston from "winston";
 import moment from "moment-timezone";
-
 const logger = winston.createLogger({
-  level: "info",
   format: winston.format.combine(
     winston.format.timestamp({
-      format: () =>
-        moment().tz("Asia/Kolkata").format("DD-MM-YYYY HH:mm:ss"),
+      format: () => moment().tz("Asia/Kolkata").format("DD-MM-YYYY HH:mm:ss"),
     }),
+    // display time according to UTC time zone
+    // winston.format.timestamp(),
     winston.format.printf(({ timestamp, level, message }) => {
       return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
     })
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/app.log" }),
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error", // only error
+    }),
+    new winston.transports.File({
+      filename: "logs/app.log",
+      level: "info", // info and above, but exclude errors via filter
+      format: winston.format.combine(
+        winston.format((info) => (info.level === "info" ? info : false))()
+      ),
+    }),
   ],
 });
 
